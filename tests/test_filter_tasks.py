35ffe68769dd6bfcf24e77ada0fb3c7720405927
denchec <dencheses@gmail.com>
2022-06-20 20:12:38 +0300

    + added more checks in test_filter_tasks.py for str_filter func + changes in ci_workflow.yml: trigger criterion python-version how to install libraries

--- tests/test_filter_tasks.py
+++ tests/test_filter_tasks.py
@@ -2,9 +2,19 @@ import pytest
 import playground.__main__ as filter_file
 
 
-@pytest.mark.parametrize(("file_name", "sub_str", "assert_equal"), [
-    ("src/playground/test.txt", "я", "Здравствуйте!\n"),
+@pytest.mark.parametrize(("iterable", "sub_str", "expected_result"), [
+    (["abc\n", "def\n", "gahi"], "a", ["def\n"]),
+    ([], "abc\n", []),
+    (["abc\n", "def\n", "ghi"], "abc\ndef\nghi", ["abc\n", "def\n", "ghi"]),
+    (["abc\n", "def\n", "ghi"], "", []),
 ])
-def test_str_filter(file_name, sub_str, assert_equal):
-    with open(file_name, "r", encoding='utf-8') as file:
-        assert "".join(list(filter_file.str_filter(sub_str, file))) == assert_equal
+def test_str_filter(iterable, sub_str, expected_result):
+    assert list(filter_file.str_filter(sub_str, iterable)) == expected_result
+
+
+@pytest.mark.parametrize(("iterable", "sub_str", "expected_result"), [
+    (["abc\n", "def\n", "ghi"], ["abc\n", "ghi"], ["def\n"]),
+])
+def test_on_fail_str_filter(iterable, sub_str, expected_result):
+    with pytest.raises(Exception):
+        assert list(filter_file.str_filter(sub_str, iterable)) == expected_result
