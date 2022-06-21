5eb2caf40001193eeefae8937d1b4c0304b00032
denchec <dencheses@gmail.com>
2022-06-21 15:40:24 +0300

    + change test_for_fail_str_filter func

--- tests/test_filter_tasks.py
+++ tests/test_filter_tasks.py
@@ -10,9 +10,9 @@ def test_str_filter(iterable, sub_str, expected_result):
     assert list(filter_file.str_filter(sub_str, iterable)) == expected_result
 
 
-@pytest.mark.parametrize(("iterable", "sub_str", "expected_result"), [
-    (["abc\n", 137, "def\n", "gahi", 9], "a", ["def\n"]),
+@pytest.mark.parametrize(("iterable", "sub_str"), [
+    (["abc\n", 137, "def\n", "gahi", 9], "a"),
 ])
-def test_for_fail_str_filter(iterable, sub_str, expected_result):
+def test_for_fail_str_filter(iterable, sub_str):
     with pytest.raises(TypeError):
-        assert list(filter_file.str_filter(sub_str, iterable)) == expected_result
+        list(filter_file.str_filter(sub_str, iterable))
